506aa915a109e8fb427151f391a6a45d7bbef724
gabrieljreed <gjr215@hercules.cs.byu.edu>
2022-09-02 10:24:07 -0600

    Finish shelfBase

--- pipe/tools/mayaTools/custom/shelfTools/shelfBase.py
+++ pipe/tools/mayaTools/custom/shelfTools/shelfBase.py
@@ -39,3 +39,28 @@ class ShelfBase(object):
             icon = self.iconPath + icon
         
         return mc.shelfButton(width=37, height=37, image=icon, l=desc, ann=desc, command=command, dcc=doubleCommand, olb=self.labelBackground, olc=self.labelColor, noDefaultPopup=True)
+    
+    def addMenuItem(self, parent, label, command=defaultNullFunc, icon=""):
+        """Adds a menu item to the specified parent menu"""
+        if icon != "":
+            icon = self.iconPath + icon
+        return mc.menuItem(label=label, command=command, image=icon, parent=parent)
+
+    def addSubMenu(self, parent, label, icon=None):
+        """Adds a sub menu to the specified parent menu"""
+        if icon != "":
+            icon = self.iconPath + icon
+        return mc.menuItem(label=label, subMenu=True, image=icon, parent=parent)
+
+    def addSeparator(self,):
+        """Adds a separator to the shelf"""
+        mm.eval("addShelfSeparator()")
+    
+    def cleanOldShelf(self):
+        """Checks if old shelf exists and empties it if it does, or create it if it does not"""
+        if mc.shelfLayout(self.name, ex=True):
+            if mc.shelfLayout(self.name, q=True, ca=True):
+                for each in mc.shelfLayout(self.name, q=True, ca=True):
+                    mc.deleteUI(each)
+        else:
+            mc.shelfLayout(self.name, p=getTopLevelShelf())
