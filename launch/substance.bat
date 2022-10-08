48192c7d13292ee67946a28807296b78229280e1
Gabriel Reed <gabrieljreed@gmail.com>
2022-10-08 13:16:45 -0600

    Add unbstance painter support

---
+++ launch/substance.bat
@@ -0,0 +1,3 @@
+set SUBSTANCE_PAINTER_PLUGINS_PATH=C:\unfamiliar_pipe\pipe\tools\substanceTools
+set SUBSTANCE_PAINTER_PLUGINS_PATH=C:\unfamiliar_pipe\pipe\tools\substanceTools
+"C:\Program Files\Adobe\Adobe Substance 3D Painter\Adobe Substance 3D Painter.exe"
