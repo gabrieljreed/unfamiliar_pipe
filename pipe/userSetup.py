c168fa84dd2a6c8fa4ae1c5904fa6d6870ed0792
Gabriel <gabrieljreed@gmail.com>
2022-10-24 09:46:24 -0600

    Add documentation comments to userSetup.py

--- pipe/userSetup.py
+++ pipe/userSetup.py
@@ -1,3 +1,5 @@
+"""This script runs on Maya startup and sets up the environment for the pipeline."""
+
 import maya.cmds as mc
 import sys
 
@@ -9,4 +11,5 @@ sys.path.append(r"/groups/unfamiliar/anim_pipeline/pipe/tools/mayaTools/custom")
 # Here we use evalDeferred to make sure we don't try to load shelves until after the UI is fully loaded
 mc.evalDeferred("import pipe.tools.mayaTools.custom.shelf as shelf")
 
+# Do the same for custom hotkeys
 mc.evalDeferred("import pipe.tools.mayaTools.UnDev.setupHotkeys as hotkeys")
