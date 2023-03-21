a7963ffa975564955f63bed83cf7c006c0cc6941
gabrieljreed <gabrieljreed@gmail.com>
2023-03-21 15:36:06 -0600

    houdini shader tools updates

---
+++ pipe/tools/houdiniTools/shading/txmake_repath_CUSTOM.py
@@ -0,0 +1,57 @@
+from fileinput import filename
+from xml.dom.expatbuilder import TEXT_NODE
+import hou 
+import os 
+
+class TxmakeRepath():
+    FILETYPE = ".png"
+    stage = hou.node("/stage/")
+    stageNodes = stage.children()
+    print(stageNodes)
+
+    def GetNodesToUpdate(self):
+        matLibNodes = []
+        #get all material library nodes in stage
+        for node in self.stageNodes:
+            if(node.type().name() == 'materiallibrary'):
+                matLibNodes.append(node)
+            
+        #for each material library node... 
+        for matLibNode in matLibNodes:
+            #get all children
+            matLibChildren = matLibNode.children()
+            
+            #determine which are pxrtextures and add to a list
+            pxrTexNodes = []
+            for node in matLibChildren:
+                if("pxrtexture::3.0" in str(node.type())):
+                    pxrTexNodes.append(node)
+                #and the normal map! 
+                elif("pxrnormalmap::3.0" in str(node.type())):
+                    pxrTexNodes.append(node)
+        return pxrTexNodes
+    
+    def UpdateFilename(self,texNode, ogFilename, newSuffix):
+        if("UDIM" not in ogFilename):
+            newFilename = ogFilename.replace(".png",newSuffix)
+        elif("UDIM" in ogFilename):
+            splitIndex = ogFilename.find(".<UDIM>")
+            suffixSplitIndex = newSuffix.find(".png")
+            finalSuffix = newSuffix[:suffixSplitIndex]+".<UDIM>"+newSuffix[suffixSplitIndex:]
+            newFilename = ogFilename[:splitIndex]+finalSuffix
+        texNode.parm("filename").set(newFilename)
+
+    def RepathTextures(self,nodesToUpdate):
+        #update each filename to point to the tex file (accounting for UDIMs) 
+        for texNode in nodesToUpdate:
+            #check that filename hasn't already been updated
+            filenameParm = texNode.parm("filename").eval()
+            if("acescg" not in filenameParm):
+                if("Normal" not in filenameParm and "SpecularRoughness" not in filenameParm):  
+                    self.UpdateFilename(texNode,filenameParm,"_srgbtex_acescg.png.tex")
+                elif("Normal" in filenameParm or "SpecularRoughness" in filenameParm):
+                    self.UpdateFilename(texNode,filenameParm,"_data_acescg.png.tex")
+                    
+myTxmakeRepath = TxmakeRepath()
+nodes = myTxmakeRepath.GetNodesToUpdate()
+myTxmakeRepath.RepathTextures(nodes)
